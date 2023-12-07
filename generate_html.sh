b25159c59a7e4caccc03dfdaedb06ebc47883e74
umbralcalc <umbralcalc@gmail.com>
2023-12-07 07:34:31 +0000

    generated index

--- generate_html.sh
+++ generate_html.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# generate home page
+pandoc --template template.html \
+--mathjax \
+-f markdown \
+-t html \
+-o index.html \
+README.md;
+
+# generate other posts
 pandoc --template template.html \
 --mathjax \
 -f markdown \
